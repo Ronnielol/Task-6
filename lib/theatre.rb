65f4e57a97a08f14bd0526f98fd0e7762e99d2f8
Aleksey Kiselev <Aleksey@AlekseyMac.local>
2017-03-03 17:18:27 +0300

    Fixes 2

--- lib/theatre.rb
+++ lib/theatre.rb
@@ -1,13 +1,15 @@
 class Theatre < MovieCollection
 
 	SCHEDULE_TIME = {morning: (6..11), afternoon: (12..17), evening: (18..23)}
-	#
+	MORNING_SETTINGS = {class: AncientMovie}
+	AFTERNOON_SETTINGS = [{genre: 'Adventure'},{genre: 'Comedy'}]
+	EVENING_SETTINGS = [{genre: 'Drama'}, {genre: 'Horror'}]
 
 	def initialize(file)
 		super(file)
-		@morning_movies = @movies.select{|movie| movie.class.to_s == 'AncientMovie'}
-		@afternoon_movies = self.filter(:genre, 'Comedy') + self.filter(:genre, 'Adventure')
-		@evening_movies = self.filter(:genre, 'Drama') + self.filter(:genre, 'Horror')
+		@morning_movies = self.filter(MORNING_SETTINGS.keys.first, MORNING_SETTINGS.values.first)
+		@afternoon_movies = self.filter(AFTERNOON_SETTINGS[0].keys.first, AFTERNOON_SETTINGS[0].values.first) + self.filter(AFTERNOON_SETTINGS[1].keys.first, AFTERNOON_SETTINGS[1].values.first)
+		@evening_movies = self.filter(EVENING_SETTINGS[0].keys.first, EVENING_SETTINGS[0].values.first) + self.filter(EVENING_SETTINGS[1].keys.first, EVENING_SETTINGS[1].values.first)
 	end
 
 	def show(time)
@@ -26,14 +28,22 @@ class Theatre < MovieCollection
 
 	def when?(movie_title)
 		selected_movie = self.filter(:title, movie_title)
-		if @morning_movies.include? selected_movie[0]
-			puts "Фильм #{movie_title} показывают утром с 6 до 11."
-		elsif @afternoon_movies.include? selected_movie[0]
-			puts "Фильм #{movie_title} показывают днем с 12 до 17"
-		elsif @evening_movies.include? selected_movie[0]
-			puts "Фильм #{movie_title} показывают вечером с 18 до 23" 	
-		else 
-			puts "В кинотеатре этот фильм не показывают."		
+		movies_in_schedule = [@morning_movies, @afternoon_movies, @evening_movies]
+		if !movies_in_schedule.inject(:+).include? selected_movie[0]
+			puts "В кинотеатре данный фильм не показывают"
+		end
+		movies_in_schedule.each do |day_part|
+			if day_part.include? selected_movie[0]
+				print "Фильм #{movie_title} показывают "
+				case day_part
+				when @morning_movies
+					puts "утром с #{SCHEDULE_TIME[:morning].first} до #{SCHEDULE_TIME[:morning].last}."	
+				when @afternoon_movies
+					puts "днем с #{SCHEDULE_TIME[:afternoon].first} до #{SCHEDULE_TIME[:afternoon].last}."
+				when @evening_movies
+					puts "вечером с #{SCHEDULE_TIME[:evening].first} до #{SCHEDULE_TIME[:evening].last}"	
+				end
+			end
 		end
 	end
 
